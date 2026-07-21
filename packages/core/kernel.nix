b92d2a3f89d7bfe0fb4203a76e953a2ee7df55b4
Tiago Dinis <tiagodinis33@gmail.com>
2026-07-21 22:16:22 +0100

    change kernel to ardos os fork

--- packages/core/kernel.nix
+++ packages/core/kernel.nix
@@ -1,10 +1,15 @@
-{ mkArdosDerivation, ap2, self, fetchurl }:
+{ mkArdosDerivation, ap2, self, writeText , fetchFromGitHub}:
 
 ap2.kernel {
-  version = "7.2-rc1";
-  src = fetchurl {
-    url = "https://git.kernel.org/torvalds/t/linux-7.2-rc1.tar.gz";
-    hash = "sha256-tGDnTPoKQoQWiBBjgh72quimpMaYkbmrEPz07fdwzg0=";
+  version = "7.2-rc4-ardos";
+  src = fetchFromGitHub {
+    owner = "ardos-os";
+    repo = "linux";
+    rev = "17caa4131897b246b5e00d36ee641c27941dab82";
+    hash = "sha256-8hx6qTYBw0tNb8przRTQZmhlOU1GRJZOY3FM9uRchaI=";
   };
-  configFile = ../../kernel/.config;
+  configFile = writeText ".config" ((builtins.readFile ../../kernel/.config)+''
+    CONFIG_EARLY_BOOT_LOGO=y
+    CONFIG_EARLY_BOOT_LOGO_FILE=${./ardosos.bmp}
+  '');
 }
