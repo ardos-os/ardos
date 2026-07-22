b24a434d27f04de2367bdc86866a2c6825f61d94
Tiago Dinis <tiagodinis33@gmail.com>
2026-07-22 20:51:35 +0100

    update kernel

--- packages/core/kernel.nix
+++ packages/core/kernel.nix
@@ -5,8 +5,8 @@ ap2.kernel {
   src = fetchFromGitHub {
     owner = "ardos-os";
     repo = "linux";
-    rev = "17caa4131897b246b5e00d36ee641c27941dab82";
-    hash = "sha256-8hx6qTYBw0tNb8przRTQZmhlOU1GRJZOY3FM9uRchaI=";
+    rev = "d767eebbb7ab793d91b611a45338a983fa8c4cf3";
+    hash = "";
   };
   configFile = writeText ".config" ((builtins.readFile ../../kernel/.config)+''
     CONFIG_EARLY_BOOT_LOGO=y
