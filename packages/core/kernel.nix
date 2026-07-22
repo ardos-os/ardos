a2ae242d611bb00363e2c7378f86c3baffdebe67
Tiago Dinis <tiagodinis33@gmail.com>
2026-07-22 20:54:38 +0100

    update kernel source hash

--- packages/core/kernel.nix
+++ packages/core/kernel.nix
@@ -6,7 +6,7 @@ ap2.kernel {
     owner = "ardos-os";
     repo = "linux";
     rev = "d767eebbb7ab793d91b611a45338a983fa8c4cf3";
-    hash = "";
+    hash = "sha256-YQPW0bVikMtsR3Z2Xb3yaxwGU1fxbw0RowQuMoBDZXE=";
   };
   configFile = writeText ".config" ((builtins.readFile ../../kernel/.config)+''
     CONFIG_EARLY_BOOT_LOGO=y
