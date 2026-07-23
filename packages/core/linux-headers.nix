f0d2e7db9cf9c827e661a4e380198e497bca2eb7
Tiago Dinis <tiagodinis33@gmail.com>
2026-07-23 14:31:36 +0100

    change linux headers to self.kernel.uapi

--- packages/core/linux-headers.nix
+++ packages/core/linux-headers.nix
@@ -1,6 +1,3 @@
 { self }:
 
-# The kernel build tree for external module development.
-# Produced by the kernel's `make all`, which builds everything including Rust
-# .rmeta files. The headers output mirrors the Arch linux-headers package.
-self.kernel.headers
+self.kernel.uapi
